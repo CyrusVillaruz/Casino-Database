3e9299cf6154cc0d43f802a5dacca33faf192f19
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-03-31 18:25:43 -0700

    Updates EMPLOYEE_ROLE table and creates insertQueries

---
+++ InsertQueries.sql
@@ -0,0 +1,45 @@
+INSERT INTO EMPLOYEE_ROLE VALUES (
+                                  'Director of Operations',
+                                  'The Director of Operations manages the ' +
+                                  'Slot Floor and a diverse team to achieve company objectives, ' +
+                                  'responsible for profit and loss, customer service scores, and ' +
+                                  'floor quality. They coordinate with various teams, lead status ' +
+                                  'update meetings, set clear goals, and provide guidance to shift ' +
+                                  'managers and support staff. They oversee regional area operations, ' +
+                                  'including adherence to project deadlines, budgets, quality, and ' +
+                                  'customer satisfaction. The role requires attendance at mandatory ' +
+                                  'outside trainings.');
+INSERT INTO EMPLOYEE_ROLE VALUES ('Shift manager', 'The Shift Manager is responsible for the overall management ' +
+                                                   'of the casino floor, including hiring and firing employees, ' +
+                                                   'ensuring that gaming regulations and laws are followed, and ' +
+                                                   'handling high-profile customers. They oversee employee training ' +
+                                                   'and development, create and maintain tracking logs, and handle ' +
+                                                   'customer complaints escalated from the supervisor level. ' +
+                                                   'Additionally, they manage money and games and provide discipline ' +
+                                                   'and development plans for employees.');
+INSERT INTO EMPLOYEE_ROLE VALUES ('Floor Supervisor', 'The Floor Supervisor is responsible for overseeing and ' +
+                                                      'managing Slot Attendants by providing on-the-job training, ' +
+                                                      'guidance, and resources to ensure adequate job performance. ' +
+                                                      'They also schedule and conduct in-house trainings and monitor ' +
+                                                      'staff schedules. The Floor Supervisor is responsible for ' +
+                                                      'maintaining a clean, safe, friendly and fun environment for ' +
+                                                      'guests and employees and ensuring superior guest service. ' +
+                                                      'They conduct pre-shift meetings with slot attendants and schedule ' +
+                                                      'mandatory regulatory online certification/re-certification for them.');
+INSERT INTO EMPLOYEE_ROLE VALUES ('Slot Attendance', 'Slot Attendance is responsible for providing customer ' +
+                                                     'service in a casino''s slot machine section. ' +
+                                                     'Their responsibilities include ensuring that slot ' +
+                                                     'machines are functioning properly, promoting special ' +
+                                                     'programs offered by the casino, making change for customers, ' +
+                                                     'and reserving slots for customers. They are also responsible for ' +
+                                                     'balancing the float at the end of the day and attending in-house ' +
+                                                     'and online trainings. A valid government-issued certification is ' +
+                                                     'required for this position.');
+INSERT INTO EMPLOYEE_ROLE VALUES ('Human Resources', 'The Human Resources department is responsible ' +
+                                                        'for various tasks related to employee management, ' +
+                                                        'including maintaining employee records and discipline ' +
+                                                        'records, creating letters of employment, issuing records ' +
+                                                        'of employment to departing employees, and tracking vacation ' +
+                                                        'and sick leave. It may also handle payroll.');
+
+SELECT * FROM EMPLOYEE_ROLE;
